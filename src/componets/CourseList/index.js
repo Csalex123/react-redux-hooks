b1f631d1cbdb7e82362033454dc087cb0024ddd7
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-08-21 23:43:03 -0300

    UseDispatch adicionado

--- src/componets/CourseList/index.js
+++ src/componets/CourseList/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+function addCourseaction(title){
+    return {type: "ADD_COURSE", title}
+}
 
 function CourseList() {
-
     const courses = useSelector(state => state.data);
+    const dispatch = useDispatch();
+
+    function addCourse(){
+        dispatch(addCourseaction('Node.js'))
+    }
 
     return (
         <>
             <ul>
                 {courses.map((course, index) => <li key={index}>{course}</li>)}
             </ul>
+
+            <button type="button" onClick={addCourse}> 
+                Adicionar curso
+            </button>
         </>
     );
 }
